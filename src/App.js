446b541afcd5c5537305aeb33040c0daf074b170
OlgaVolos <72765623+OlgaVolos@users.noreply.github.com>
2021-04-05 00:09:39 +0300

    lesson4_unControl

--- src/App.js
+++ src/App.js
@@ -5,70 +5,70 @@ const BASE_URL = `https://jsonplaceholder.typicode.com`
 
 function App() {
 
-  const inputEndpoint = useRef();
-  const inputId = useRef();
+    const inputEndpoint = useRef();
+    const inputId = useRef();
 
-const [items, setItems] = useState([])
-  const [singleItem, setSingleItem] = useState()
-  let [endpoint, setEndpoint] = useState('')
-  let [id, setId] = useState('')
-  const [error, setError] = useState('')
+    const [items, setItems] = useState([])
+    const [singleItem, setSingleItem] = useState()
+    let [endpoint, setEndpoint] = useState('')
+    let [id, setId] = useState('')
+    const [error, setError] = useState('')
 
-  const fetchData = async () => {
-    const response = await fetch(`${BASE_URL}/${endpoint}/${id}`);
-    const data = await response.json();
-    if (id){
-      setSingleItem(data);
-      setItems([]);
-      return
+    const fetchData = async () => {
+        const response = await fetch(`${BASE_URL}/${endpoint}/${id}`);
+        const data = await response.json();
+        if (id) {
+            setSingleItem(data);
+            setItems([]);
+            return
+        }
+        setSingleItem(null);
+        setItems(data);
     }
-    setSingleItem(null);
-    setItems(data);
-  }
-  const onSubmit = (e) => {
-  e.preventDefault();
-  endpoint = inputEndpoint.current.value;
-  id = inputId.current.value;
-  if(!endpoint){
-    return setError('empty field')
-  }
-  if(!END_POINT.includes(endpoint.toLowerCase().trim())) {
-    return setError("write one of list: 'posts', 'comments', 'albums', 'photos', 'todos', 'users' ")
-  }
-  const idToNum = Number(id);
-  if(!idToNum && id !=='' && idToNum !== 0){
-    return setError('use numerical value from 1 to 100')
-  }
-  if((idToNum<1 || idToNum>100) && id !== ''){
-    return setError('"write from 1 to 100"')
-  }
-    fetchData();
-  setError('')
-}
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-      <input ref={inputEndpoint} type="text" placeholder='type some text' name="endpoint"/>
-      <br/>
-      <input ref={inputId} type="text" placeholder='type some number' name="id"/>
-      <br/>
-      <button type='submit'>Submit</button>
-      </form>
+    const onSubmit = (e) => {
+        e.preventDefault();
+        endpoint = inputEndpoint.current.value;
+        id = inputId.current.value;
+        if (!endpoint) {
+            return setError('empty field')
+        }
+        if (!END_POINT.includes(endpoint.toLowerCase().trim())) {
+            return setError("write one of list: 'posts', 'comments', 'albums', 'photos', 'todos', 'users' ")
+        }
+        const idToNum = Number(id);
+        if (!idToNum && id !== '' && idToNum !== 0) {
+            return setError('use numerical value from 1 to 100')
+        }
+        if ((idToNum < 1 || idToNum > 100) && id !== '') {
+            return setError('"write from 1 to 100"')
+        }
+        fetchData();
+        setError('')
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input ref={inputEndpoint} type="text" placeholder='type some text' name="endpoint"/>
+                <br/>
+                <input ref={inputId} type="text" placeholder='type some number' name="id"/>
+                <br/>
+                <button type='submit'>Submit</button>
+            </form>
 
-      <div style={{color: 'red'}}>
-        {error}
-      </div>
+            <div style={{color: 'red'}}>
+                {error}
+            </div>
 
-      <div>
-        {singleItem && JSON.stringify(singleItem, null, 2)}
-      </div>
+            <div>
+                {singleItem && JSON.stringify(singleItem, null, 2)}
+            </div>
 
-      <div>
-        {items.map(el => (<div key={el.id}>{el.id} <br/> {el.name} <br/> {el.title} <br/> {el.email} </div>))}
-      </div>
+            <div>
+                {items.map(el => (<div key={el.id}>{el.id} <br/> {el.name} <br/> {el.title} <br/> {el.email} </div>))}
+            </div>
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
