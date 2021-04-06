2d96d8de304b8e15532081b0bedb55e2039e059f
OlgaVolos <72765623+OlgaVolos@users.noreply.github.com>
2021-04-06 16:55:53 +0300

    lesson5

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from "./components/Header/Header";
 
 function App(props) {
   return (
-      <div></div>
+      <div>
+        <Header/>
+      </div>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './header.css'
+function Header (props) {
+    return (
+        <div className='header'></div>
+    );
+}
+
+export default Header;
