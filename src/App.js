4e898a39b8eff69b1f5f9d8cb80166c123befec6
OlgaVolos <72765623+OlgaVolos@users.noreply.github.com>
2021-04-10 18:22:33 +0300

    lesson6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, {useState, createContext, useContext} from "react";
 
-function App(props) {
+const CounterContext = createContext();
+console.log(CounterContext);
+
+const ContextProvider = ({children}) => {
+  const [counter, setCounter] = useState(0);
+
+  const incCounter = () => {
+    setCounter(counter+1)
+  };
+
+  const decCounter = () => {
+    setCounter(counter-1)
+  };
+  return(
+      <CounterContext.Provider value={{counter,
+        incCounter,
+        decCounter,}}>
+        {children}
+      </CounterContext.Provider>
+
+  )
+};
+
+const Counter = () => {
+    const {counter, decCounter} = useContext(CounterContext);
+
+
   return (
-      <div></div>
-  );
-}
+      <div>
+      <h1>Counter: {counter}</h1>
+          <button onClick={decCounter}>DEC</button>
+        </div>
+  )
+};
+const Header = () => {
+    const {counter, incCounter} = useContext(CounterContext);
 
-export default App;
+// const counterContext = useContext(CounterContext); замість counterContext,
+// ми можемо деструктуризувати і витягнути те, що лежить в ньому
+//   console.log(counterContext);
+  return(
+      <h3 onClick={incCounter} style={{color: 'red'}}>Header: {counter} </h3>
+  )
+
+};
+
+export default function App() {
+
+
+  return(
+      <ContextProvider>
+        <Counter  />
+        <Header />
+</ContextProvider>
+  )
+};
